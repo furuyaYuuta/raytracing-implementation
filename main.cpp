597e8e1838447f91b382409f068132e36dff71ef
furuyaYuuta <mochi77.uni@gmail.com>
2020-04-07 06:45:19 +0800

    complete image output

--- main.cpp
+++ main.cpp
@@ -1,10 +1,47 @@
 
 #include <iostream>
+#include <cstdint>
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb/stb_image_write.h"
 
 using namespace std;
 
-int main() {
+class Image {
+	int width, height;
+	uint8_t *data;
+public:
+	Image(int width, int height) {
+		this->width = width;
+		this->height = height;
+		data = new uint8_t[width * height * 3];
+	}
+	void setPixel(int x, int y, int r, int g, int b) {
+		data[3 * (width * y + x)] 	  = (uint8_t)r;
+		data[3 * (width * y + x) + 1] = (uint8_t)g;
+		data[3 * (width * y + x) + 2] = (uint8_t)b;
+	}
+	void writeImage() const {
+		stbi_write_png("image.png", width, height, 3, data, 3 * width);
+	}
+};
 
+int main() {
+	int nx = 800, ny = 400;
+	Image image(nx, ny);
+
+	for(int j = ny - 1; j >= 0; j--) {
+		for(int i = 0; i < nx; i++) {
+			float r = float(i) / float(nx);
+			float g = float(j) / float(ny);
+			float b = 0.2;
+			int ir = int(255.99 * r);
+			int ig = int(255.99 * g);
+			int ib = int(255.99 * b);
+			image.setPixel(i, ny - j - 1, ir, ig, ib);
+		}
+	}
+	image.writeImage();
 
 	return 0;
 }
